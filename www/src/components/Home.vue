090b795399854561f45568dc1ab138bcb55f19d4
cody budge <codybudge@gmail.com>
2018-06-02 12:08:00 -0600

    final css changes

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -5,26 +5,26 @@
         <div class="col d-flex justify-content-center">
           <!--Serch Bar-->
           <form @submit.prevent="findSongs">
-            <input type="text" v-model="query">
+            <input type="text" placeholder="Search for HotLineMusic" v-model="query">
+            
           </form>
-          <button @click="findSongs">Search</button>
+          <button class="but" @click="findSongs"><i class="fa fa-fw fa-search"></i></button>
           <div class="results">
-            <h4>Results for: {{query}}</h4>
           </div>
         </div>
       </div>
-
+      
       <div class="row">
         <div class="col d-flex justify-content-center">
-          <div class="card" style="width: 27rem;">
+          <div class="songoutercard" style="width: 27rem;">
               <h3>Songs</h3>
-          <div class="card-body">
-            <div class="card" style="width: 24rem;">
+          <div class="card-body d-flex justify-content-center">
+            <div class="songinnercard" style="width: 24rem;">
               <div class="card-body">
                 <ol>
-                  <li v-for="song in songs">
-                    <img :src="song.albumArt" alt="">
-                    <p> Song Title:{{song.title}}</p>
+                  <li v-for="song in songs" class="list">
+                    <img class="img" :src="song.albumArt" alt="">
+                    <h2> Song Title:{{song.title}}</h2>
                     <p> Artist Name:{{song.artist}}</p>
                     <p> Album Name: {{song.album}}</p>
                     <p>Cost: ${{song.price}}</p>
@@ -32,7 +32,7 @@
                       <source :src="song.preview">
                       <type="audio/wav">
                     </audio>
-                    <button @click="addToPlaylist(song)">Add to playList</button>
+                    <button class="but" @click="addToPlaylist(song)">Add to playList</button>
                   </li>
                 </ol>
               </div>
@@ -44,25 +44,25 @@
 
           <div class="col d-flex justify-content-center">
             <!--Your Play list side-->
-            <div class="card" style="width: 27rem;">
+            <div class="playoutercard  " style="width: 27rem;">
                 <h3>My Playlist</h3>
-              <div class="card-body">
+              <div class="card-body d-flex justify-content-center">
 
-                <div class="card" style="width: 24rem;">
-                  <div class="card-body">
-                    <ol class="list">
-                      <li v-for="(song, index) in playlist.songs">
-                        <img :src="song.albumArt" alt="">
-                        <p> Song Title:{{song.title}}</p>
-                        <button @click="moveUp(index)">move up</button>
-                        <button @click="moveDown(index)">move down</button>
+                <div class="playinnercard" style="width: 24rem;">
+                  <div class="card-body ">
+                    <ol class="unlist">
+                      <li v-for="(song, index) in playlist.songs" class="list">
+                        <img class="img" :src="song.albumArt" alt="">
+                        <h2> Song Title:{{song.title}}</h2>
+                        <button class="but" @click="moveUp(index)">move up</button>
+                        <button class="but" @click="moveDown(index)">move down</button>
                         <p> Artist Name:{{song.artist}}</p>
                         <p> Album Name: {{song.album}}</p>
                         <audio class="audioBar" controls="controls">
                           <source :src="song.preview">
                           <type="audio/wav">
                         </audio>
-                        <button @click="removeSong(song._id)">Remove</button>
+                        <button class="but" @click="removeSong(song._id)">Remove</button>
                       </li>
                     </ol>
                   </div>
@@ -129,5 +129,39 @@
 <style scoped>
   h3{
     text-align: center
+  }
+  .container-fluid {
+   background: linear-gradient(#374955,#203541);
+   font-family: 'Fjalla One', sans-serif;
+  }
+  .playoutercard{
+    text-align: center;
+    background: linear-gradient(#f62a66,#a11640);
+    border-radius: 10px 50px 10px 20px
+  }
+  .playinnercard{
+    background: linear-gradient(#ffd933,#e08815)
+  }
+  .songoutercard{
+    text-align: center;
+    background: linear-gradient(#f62a66,#a11640);
+    border-radius: 50px 10px 20px 10px
+  }
+  .songinnercard{
+    background: linear-gradient(#ffd933,#e08815)
+  }
+  .list{
+    margin-bottom: 2rem;
+    margin-top: 2rem
+  }
+  .img{
+    border: solid .3rem rgb(51, 51, 51);
+    border-radius: 1rem
+  }
+  .but{
+    border-radius: 2rem;
+    background: #a11640;
+    border: solid .3rem rgb(51, 51, 51);
+    color: white
   }
 </style>
